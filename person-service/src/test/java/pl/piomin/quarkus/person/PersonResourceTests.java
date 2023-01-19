97c94b5aa8409af0e04d8b8c81777d6e030ff0e8
piomin <piotr.minkowski@gmail.com>
2023-01-19 12:23:17 +0100

    adding tests for `person-service` + fixing bugs

---
+++ person-service/src/test/java/pl/piomin/quarkus/person/PersonResourceTests.java
@@ -0,0 +1,35 @@
+package pl.piomin.quarkus.person;
+
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
+import org.instancio.Instancio;
+import org.instancio.Select;
+import org.junit.jupiter.api.Test;
+import pl.piomin.quarkus.person.model.Person;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+
+@QuarkusTest
+public class PersonResourceTests {
+
+    @Test
+    void getAll() {
+        given().get("/persons")
+                .then()
+                .statusCode(200)
+                .assertThat().body("size()", is(6));
+    }
+
+    @Test
+    void add() {
+        Person person = Instancio.of(Person.class)
+                .ignore(Select.field("id"))
+                .create();
+        given().body(person).contentType(ContentType.JSON)
+                .post("/persons")
+                .then()
+                .statusCode(200);
+    }
+}
